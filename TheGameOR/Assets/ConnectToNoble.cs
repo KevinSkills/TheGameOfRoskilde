89cfdfa0375635a7b41a04df9c1c954fd40f65ed
KevinSkills <kevin.lock03@gmail.com>
2022-04-21 09:16:26 +0200

    Made client and host connection through db

--- TheGameOR/Assets/ConnectToNoble.cs
+++ TheGameOR/Assets/ConnectToNoble.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 using NobleConnect.Mirror;
 using Mirror;
+using UnityEngine.Networking;
 
 public class ConnectToNoble : MonoBehaviour
 {
@@ -24,6 +25,8 @@ public class ConnectToNoble : MonoBehaviour
     // Used to determine which GUI to display
     bool isHost, isClient;
 
+    bool connected;
+
     public void Start()
     {
         // Cast from Unity's NetworkManager to a NobleNetworkManager.
@@ -33,24 +36,96 @@ public class ConnectToNoble : MonoBehaviour
 
     public void startHost()
     {
+        connected = false;
         isHost = true;
         isClient = false;
 
         networkManager.StartHost();
 
+
     }
 
     public void startClient()
+    {
+        //if no adress given, then get it 
+        StartCoroutine(IE_startClientFromDB());
+
+    }
+
+    IEnumerator IE_startClientFromDB()
+    {
+        //Get ip and port from database (do this with webrequest)
+        WWWForm form = new WWWForm();
+
+        form.AddField("playerID", nfcIDToJoin.text);
+
+        UnityWebRequest uwr = UnityWebRequest.Post("https://thegameor.000webhostapp.com/get.php", form);
+
+        yield return uwr.SendWebRequest();
+        string dataText = uwr.downloadHandler.text;
+
+        if (dataText[0].Equals('0'))
+        {
+
+            //if it worked, then startclient with data:
+
+            Debug.Log(dataText);
+
+            string[] splitData = dataText.Remove(0).Split(':'); //array with ip on 0 and port on 1
+
+            startClient(splitData[0], ushort.Parse(splitData[0]));
+        }
+        else
+        {
+            print("ERROR: " + dataText);
+        }
+    }
+
+
+    IEnumerator sendAdressToDB(string givenIp, string givenPort)
+    {
+        
+        WWWForm form = new WWWForm();
+
+        form.AddField("playerID", nfcID.text);
+        form.AddField("ip", givenIp);
+        form.AddField("port", givenPort);
+
+        UnityWebRequest uwr = UnityWebRequest.Post("https://thegameor.000webhostapp.com/set.php", form);
+
+        yield return uwr.SendWebRequest();
+        string dataText = uwr.downloadHandler.text;
+
+        if (dataText[0].Equals('0'))
+        {
+
+            //if it worked, then startclient with data:
+
+            Debug.Log("worked");
+
+
+        }
+        else
+        {
+            print("ERROR: " + dataText);
+            Disconnect();
+        }
+    }
+
+    public void startClient(string givenIp, int givenPort)
     {
         //Initialize client
         networkManager.InitClient();
         isHost = false;
         isClient = true;
 
-        //Get ip and port from database
-        networkManager.networkAddress = "getIpFromDatabase()";
-        networkManager.networkPort = ushort.Parse("getPortFromDatabase()");
-        
+
+
+        //set the ip and port in the network manager
+        networkManager.networkAddress = givenIp;
+        networkManager.networkPort = givenPort;
+
+
         //start client
         networkManager.StartClient();
     }
@@ -66,7 +141,13 @@ public class ConnectToNoble : MonoBehaviour
                 ip = networkManager.HostEndPoint.Address.ToString();
                 port = networkManager.HostEndPoint.Port.ToString();
                 status.text = "Connected :D - ip: " + ip + ":" + port;
-                //database tilføj her
+                
+                if (!connected)
+                {
+                    connected = true;
+                    StartCoroutine(sendAdressToDB(ip, port));
+
+                }
             }
 
             
@@ -112,6 +193,11 @@ public class ConnectToNoble : MonoBehaviour
             isClient = false;
         }
     }
+
+
+
+
+
 
 
 
--- TheGameOR/Assets/Scenes/Database.cs
+++ TheGameOR/Assets/Scenes/Database.cs
@@ -6,11 +6,14 @@ using UnityEngine.Networking;
 
 public class Database : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         
-        StartCoroutine(register());
+        
     }
 
     // Update is called once per frame
@@ -20,32 +23,6 @@ public class Database : MonoBehaviour
         
     }
 
-    IEnumerator register() {
-        WWWForm form = new WWWForm();
-        form.AddField("port", "john");
-        form.AddField("ip", "123");
-        form.AddField("playerID", "121223");
 
-        UnityWebRequest uwr = UnityWebRequest.Post("https://thegameor.000webhostapp.com/get.php", form);
-        
-        
-
-        
-
-
-        yield return uwr.SendWebRequest();
-        string dataText = uwr.downloadHandler.text;
-
-        if (dataText.Equals("0"))
-        {
-            print("hej");
-        }else
-        {
-            print("ERROR: " + dataText);
-        }
-
-    
-    
-    }
 
 }
